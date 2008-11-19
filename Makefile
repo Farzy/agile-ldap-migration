30389beaf9c4927ceb6f24c3dd4823d8d0199bf1
Farzad FARID <ffarid@pragmatic-source.com>
2008-11-19 23:41:38 +0000

    Script de migration LDAP pour IDM
    Conversion et nettoyage d'une base OpenLDAP 2.1 vers la version 2.3.
    Première version de travail. Il reste des nettoyage LDIF à écrire.

---
+++ Makefile
@@ -0,0 +1,31 @@
+INFILE	:= idm-orig-20081022.ldif
+OUTFILE := idm-clean.ldif
+CLEANER := ./idm-clean-ldif.rb
+ROOTDN  := "cn=admin, o=idm, c=fr"
+ROOTPW  := "Ur,ec1blC"
+
+all:
+	@echo "Vous devez specifier la commande a executer"
+	exit 1
+
+clean_ldif:
+	$(CLEANER) $(INFILE) > $(OUTFILE)
+
+test: clean_ldif
+	ldapadd -n -x -h localhost -D $(ROOTDN) -w $(ROOTPW) -f $(OUTFILE)
+
+# Attention : cette commande détruit le contenu de l'annuaire
+insert_incr: clean_ldif
+	ldapadd -c -x -h localhost -D $(ROOTDN) -w $(ROOTPW) -f $(OUTFILE)
+
+# Attention : cette commande détruit le contenu de l'annuaire
+empty_directory:
+	/etc/init.d/slapd stop
+	sleep 1
+	rm -f /var/lib/ldap/*
+	/etc/init.d/slapd start
+
+# Attention : cette commande détruit le contenu de l'annuaire
+insert_full: clean_ldif empty_directory
+	sleep 2
+	ldapadd -c -x -h localhost -D $(ROOTDN) -w $(ROOTPW) -f $(OUTFILE)
---
+++ lib/idm-clean-ldif.rb
@@ -0,0 +1,199 @@
+#!/usr/bin/env ruby
+# -*- coding: None -*-
+# vim: sw=2 sts=2:
+
+# idm-clean-ldif.rb : Nettoyage et mise en confirmité de l'annuaire
+# LDAP IDM pour OpenLDAP 2.3
+
+# Auteur:: Farzad FARID
+# Copyright:: (C) 2008 Pragmatic Source
+#
+# = Licence
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# 
+# = Synopsys
+# 
+# Ce programme prend en entrée un fichier LDIF au format OpenLDAP 2.0,
+# avec les objets dans le désordres et des attributs manquants, et ressort
+# un fichier LDIF correct.
+
+# Passage en UTF-8
+$KCODE = 'u'
+require 'jcode'
+
+require 'rubygems'
+require 'ldap'
+require 'ldap/ldif'
+require 'trollop'
+require 'pp'
+
+PROGNAME = "idm-clean-ldif"
+VERS = "1.0"
+
+class LdapCleaner
+  attr_accessor :infile, :ldif
+
+	# Trouve le RDN dans un DN bien formé
+	RDN_REG = /^(.*?)=(.*?),.*$/
+
+  # Ces fonctions sont inspirées de LDAP::LDIF
+
+  LINE_LENGTH = 77
+
+  # return *true* if +str+ contains a character with an ASCII value > 127 or
+  # a NUL, LF or CR. Otherwise, *false* is returned.
+  #
+  def self.unsafe_char?( str )
+    # This could be written as a single regex, but this is faster.
+    str =~ /^[ :]/ || str =~ /[\x00-\x1f\x7f-\xff]/
+  end
+
+  # Perform Base64 decoding of +str+. If +concat+ is *true*, LF characters
+  # are stripped.
+  #
+  def self.base64_encode( str, concat=false )
+    str = [ str ].pack( 'm' )
+    str.gsub!( /\n/, '' ) if concat
+    str
+  end
+
+  # Convert an attribute to LDIF
+  # 
+  # * attr: attribute name
+  # * vals: a hash of values
+  def self.attr_to_ldif(attr, vals)
+    ldif_string = ''
+
+    vals.each do |val|
+      val = val.clone # Essentiel
+      sep = ':'
+      if unsafe_char?( val )
+        sep = '::'
+        val = base64_encode( val, true )
+      end
+
+      firstline_len = LINE_LENGTH - ( "%s%s " % [ attr, sep ] ).length
+      ldif_string << "%s%s %s\n" % [ attr, sep, val.slice!( 0..firstline_len ) ]
+
+      while val.length > 0
+        ldif_string << " %s\n" % val.slice!( 0..LINE_LENGTH - 1 )
+      end
+    end
+
+    ldif_string
+  end
+
+  # Convert an LDAP::Record to LDIF.
+  # Cleanup Schema errors.
+  def self.to_ldif(ldap_record)
+    # Certains DN contiennent un retour chariot, à corriger
+    dn = ldap_record.dn.gsub(/\n/, '')
+    attrs = ldap_record.attrs
+
+    ldif_string = "dn: %s\n" % dn
+
+    # Le CN présent dans les attributs est parfois erroné, et
+    # différent du RDN. Autant forcer sa valeur avant insertion
+    md = RDN_REG.match(dn)
+    rdn_name  = md[1]
+    rdn_value = md[2]
+    attrs[rdn_name] = rdn_value
+
+    # Entrée de test à supprimer, car elle est incorrecte
+    return if dn == "cn=test,o=idm,c=fr"
+    
+    # Entrées de type societyUnit (su), enregistrée avec l'OC
+    # "ou" par erreur. On force l'attribut, au cas où il
+    # n'existe pas
+    # On doit en plus tenir compte de plusieurs casses pour "societyUnit"
+    if rdn_name == "su" # and !( attrs["objectclass"].include?("societyUnit") || attrs["objectclass"].include?("SocietyUnit"))
+      attrs["objectclass"] -= [ "societyUnit", "SocietyUnit", "organizationalUnit" ]
+      attrs["objectclass"] << "societyUnit"
+    end
+
+    # su=glucoz.com contient un attribut "ou" qu'il faut supprimer
+    if /^su=glucoz\.com/.match(dn)
+      attrs.delete("ou")
+    end
+    
+    # Pour certains RDN "ou=", l'object class organizationalUnit correspondant
+    # est manquant
+    if 
+    attrs.each do |attr,vals|
+      ldif_string << attr_to_ldif(attr, vals)
+    end
+
+    ldif_string
+    #LDIF::Entry.new( ldif )
+  end
+
+  #--------------------
+
+  def initialize
+  end
+
+  def parse_arguments
+    opts = Trollop::options do
+      version "#{PROGNAME} #{VERS} (c) 2008 Farzad FARID"
+      banner <<-EOS
+Convertit un fichier LDAP au format LDIF d'un version ancienne et buggée
+d'OpenLDAP en un fichier LDIF compatible avec OpenLDAP 2.3.
+
+Usage: #{PROGNAME} [options] <Fichier LDIF>
+EOS
+      opt :debug, "Mode debug", :default => false
+    end
+
+    if ARGV.size != 1 || !File.exist?(ARGV[0])
+      Trollop::die "Fichier LDIF manquant ou trop de paramètres"
+    end
+    self.infile = ARGV[0]
+  end
+
+  def read_ldif(filename = nil)
+    self.infile ||= filename
+    # Lecture du fichier LDIF et tri des enregistrements
+    self.ldif = LDAP::LDIF::parse_file(infile, true)
+    # Les données peut être volumineuses, on ne veut pas que ça
+    # puisse s'afficher en cas d'utilisation de ce script sous "irb"..
+    nil
+  end
+
+  def clean_ldif
+    output = ''
+
+    ldif.each { |rec|
+      # On enlève les attributs système
+      rec.clean
+      # Cette fonction fait un gros nettoyage
+      obj = LdapCleaner::to_ldif(rec)
+      output << obj << "\n" if !obj.nil?
+    }
+    output
+  end
+
+  def run
+    parse_arguments
+    read_ldif
+    data = clean_ldif
+    puts data
+  end
+
+end
+
+if __FILE__ == $0
+  app = LdapCleaner.new
+  app.run
+end
